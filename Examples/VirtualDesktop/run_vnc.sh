d4921e04960b3320847c6bfa4c5f461a6da1c71a
Stefano Alberto Russo <stefano.russo@gmail.com>
2017-09-27 10:41:12 +0200

    Added Virtual Desktop example

---
+++ Examples/VirtualDesktop/run_vnc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Exec TigerVNC server as this instance user
+sudo -i -u ubuntu /opt/tigervnc/usr/bin/vncserver :0 -SecurityTypes None -xstartup /opt/tigervnc/xstartup
+
+# Check it is running and if no, exit
+while true
+do
+
+    PSOUT=$(ps -ef | grep /opt/tigervnc/usr/bin/Xvnc | grep SecurityTypes) 
+
+    if [[ "x$PSOUT" == "x" ]] ; then
+        exit 1
+    fi
+
+	# Sleep other 10 secs before re-checking
+	sleep 10
+
+done
